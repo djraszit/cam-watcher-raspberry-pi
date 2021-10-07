f98a8e8586b1e1fd35dc2d98a53018795801255f
raszit <djraszit@gmail.com>
2021-10-07 14:05:12 +0200

    Dodanie nowej funkcji: Zmiana wyświetlanego kanału przez socket
    Test zmian

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ else
 fi
 
 #pliki do skopiowania
-FILES=(display-cameras.sh cam-switcher.sh cam_position_src.sh omxplayer-dbus-control.sh cam_num)
+FILES=(display-cameras.sh cam-switcher.sh cam_position_src.sh omxplayer-dbus-control.sh socket-cam-switcher.sh cam_num)
 
 for i in ${FILES[@]};do
 	if [ -e $INSTALL_DIR/$i ];then
@@ -65,6 +65,8 @@ done
 SYSTEMD_DIR=/etc/systemd/system
 SCRIPTS=("display-camera@.service")
 SCRIPTS+=("cam-switcher.service")
+SCRIPTS+=("socket-cam-switcher@.service")
+SCRIPTS+=("socket-cam-switcher.socket")
 
 for i in ${SCRIPTS[@]};do
 	if [ -e $SYSTEMD_DIR/$i ];then
@@ -85,10 +87,17 @@ sudo sed -i '/ExecStart/d' $SYSTEMD_DIR/cam-switcher.service
 
 sudo sed -i '/Type=forking/a\ExecStart='"$INSTALL_DIR/cam-switcher.sh" $SYSTEMD_DIR/cam-switcher.service
 
+sudo sed -i '/ExecStart/d' $SYSTEMD_DIR/socket-cam-switcher@.service
+
+sudo sed -i '/Type=simple/a\ExecStart='"$INSTALL_DIR/socket-cam-switcher.sh" $SYSTEMD_DIR/socket-cam-switcher@.service
+
 sudo systemctl daemon-reload
 
 sudo systemctl enable cam-switcher.service
 sudo systemctl start cam-switcher.service
+
+sudo systemctl enable socket-cam-switcher.socket
+sudo systemctl start socket-cam-switcher.socket
 
 sudo systemctl enable display-camera@ch1.service
 sudo systemctl start display-camera@ch1.service
---
+++ to_install/socket-cam-switcher.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
+
+cam_num_file="$DIR/cam_num"
+source $DIR/cam_position_src.sh
+
+chname=("Front")
+chname+=("Back")
+chname+=("Brama")
+chname+=("Balkon")
+
+
+
+
+cam_on_top=0
+cam_num=4
+
+if [ -e $cam_num_file ];then
+	temp=$(cat $cam_num_file)
+	if [ "${temp:0:2}" == "ch" ];then
+		cam_on_top=${temp:2}
+	fi
+	if [ "$temp" == "allcam" ];then
+		cam_on_top=0
+	fi
+fi
+
+
+while read -d . SOCKET; do
+	case $SOCKET in
+		next)
+		cam_on_top=$((cam_on_top + 1));
+		if [ $cam_on_top -gt $cam_num ];then
+			cam_on_top=0
+		fi
+		;;
+		prev)
+		cam_on_top=$((cam_on_top - 1));
+		if [ $cam_on_top -lt 0 ];then
+			cam_on_top=$cam_num
+		fi
+		;;
+
+	esac
+
+	if [ $cam_on_top -gt 0 ];then
+		for ((l = 1; l <= $cam_num; l++));do			
+			$DIR/omxplayer-dbus-control.sh ch$l hidevideo
+			$DIR/omxplayer-dbus-control.sh ch$l setvideopos $full_screen
+		done
+		for ((l = 1; l <= $cam_num; l++));do
+			if [ $l -eq $cam_on_top  ];then
+				$DIR/omxplayer-dbus-control.sh ch$l setlayer 5
+				$DIR/omxplayer-dbus-control.sh ch$l unhidevideo
+			else
+				$DIR/omxplayer-dbus-control.sh ch$l setlayer 1
+			fi
+		done
+		text="ch$cam_on_top"
+		send="0 10 60 ${chname[cam_on_top - 1]}!\n"
+	else
+		$DIR/omxplayer-dbus-control.sh ch1 setvideopos $top_left_pos
+		$DIR/omxplayer-dbus-control.sh ch2 setvideopos $top_right_pos
+		$DIR/omxplayer-dbus-control.sh ch3 setvideopos $bottom_left_pos
+		$DIR/omxplayer-dbus-control.sh ch4 setvideopos $bottom_right_pos
+		for ((l = 1; l <= $cam_num; l++));do
+			$DIR/omxplayer-dbus-control.sh ch$l unhidevideo
+		done
+		text="allcam"
+		send="0 5 40 ${chname[0]}!\n960 5 40 ${chname[1]}!\n0 548 40 ${chname[2]}!\n960 548 40 ${chname[3]}!\n"
+	fi
+
+	echo "$text" > $DIR/cam_num
+	echo -e "$send" > /tmp/hello_font_server
+	echo -e "$send" | socat STDIO UNIX-CONNECT:/tmp/hello_font_by_raszit,connect-timeout=1
+
+done
+
+exit 0
+
